5d4fe8bc754cde340150c3e3653068327dad1037
nejira-k <kadric.nejira@gmail.com>
2021-03-11 19:54:52 +0100

    Added new files

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    
+    var myFirstName = "Nejira";
+    var myLastName = "Kadric";
+    var myHomeTown = "Gracanica";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
